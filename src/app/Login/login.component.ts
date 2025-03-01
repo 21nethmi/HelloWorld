dc6fdf1e63c741a30c53ca4c3dcdd6c0876ed4f6
21nethmi <nethminavoda20020821@gmail.com>
2025-03-01 17:56:27 +0530

    trial commit

---
+++ src/app/Login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+@Component({
+    selector : 'app-login',
+    standalone:true,
+    // template:`<h1>Login component</h1>
+    //  <p>NEmo</p>
+    //  <p>heloo</p>`,
+     templateUrl:'login.component.html',
+    //  styles:`h1{color:red ;background-color:blue}
+    //  p{color:blue}`
+    styleUrl:'login.component.css'
+
+
+})
+ export class LoginComponent{
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./Login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, DashboardComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  inputValue:String='';
+  title:String = 'HelloWorld';
+  imageUrl:String = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbOFmjGchTMwQriXqezOovYKqXWK3YXUnFlQ&s';
+
+  isDisable:boolean=true;
+
+  ariaLabelTxt = "this is a button,when clicking it open";
+  colSpan=2;
+  color="blue";
+  isActive:boolean=false;
+  name:string="nemo1";
+  clickNote:string="clickeddd";
+  twoWayVal:string='';
+
+  buttonClicked(){
+   console.log("Button clicked");
+   this.clickNote="clickeeedd";
+  }
+
+  inputEvent(event:KeyboardEvent){
+    console.log(event.key);
+    console.log(event.keyCode);
+    if(event.keyCode == 13){
+      console.log("Enter key pressed");
+    }
+  }
+  KeyDownClicked(){
+         console.log("Enter clicked");
+  }
+
+  keyPressed(input:HTMLInputElement){
+    this.inputValue=input.value;
+    console.log(input.value);
+  }
 }
+
+
+
